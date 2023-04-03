4608e9d1c46a60ec0050969c3b76d5dc78f05f7a
Ben <ben_m_thomas@uri.edu>
2023-04-03 10:14:08 -0400

    added bitpack - Ben

---
+++ bitpack/target/debug/deps/bitpack-3cdcb763b0f2a39a.d
@@ -0,0 +1,6 @@
+c:\Users\benmt\CSC411\arith\bitpack\target\debug\deps\bitpack-3cdcb763b0f2a39a.rmeta: src\lib.rs src\bitpack.rs
+
+c:\Users\benmt\CSC411\arith\bitpack\target\debug\deps\bitpack-3cdcb763b0f2a39a.d: src\lib.rs src\bitpack.rs
+
+src\lib.rs:
+src\bitpack.rs:
---
+++ bitpack/target/debug/deps/bitpack-67442f3115831fd6.d
@@ -0,0 +1,6 @@
+c:\Users\benmt\CSC411\arith\bitpack\target\debug\deps\bitpack-67442f3115831fd6.rmeta: src\lib.rs src\bitpack.rs
+
+c:\Users\benmt\CSC411\arith\bitpack\target\debug\deps\bitpack-67442f3115831fd6.d: src\lib.rs src\bitpack.rs
+
+src\lib.rs:
+src\bitpack.rs:
